8871235a57dde2e6dd51982bb7017d260c8d66e3
Gordon Krefting <gordon@krefting.org>
2023-08-30 15:33:01 -0400

    Much better OO, better tests, added more data elements

--- book_formatter/book_formatter/book_formatter.py
+++ book_formatter/book_formatter/book_formatter.py
@@ -3,33 +3,89 @@ import os
 
 
 class PhotoRecord:
-    scientific_name = None
-    common_name = None
-
     def __init__(self, r):
         if 'scientificName' in r and r['scientificName'] != '?':
             self.scientific_name = r['scientificName']
+        else:
+            self.scientific_name = None
         self.common_name = r.get('commonName')
+        self.plant_type = r.get('plantType')
+        self.location = r.get('location')
         self.rating = r.get('rating', 0)
+        self.invasive = r.get('invasive', 'No')
         self.filename = os.path.splitext(r['fileName'])[0] + '.jpg'
 
 
 class PlantRecord:
-    scientific_name = None
-    common_name = None
-    invasive = None
-    plant_type = None
-    location = set()
-    photos = []
-    errors = []
-
     def __init__(self, records):
         self.scientific_name = records[0].scientific_name
-        records.sort(key=lambda r: r['rating'])
+        self.common_name = None
+        self.invasive = False
+        self.plant_type = None
+        self.locations = set()
+        self.photos = []
+        self.errors = set()
+
+        records.sort(key=lambda r: r.rating, reverse=True)
         for r in records:
-            self.common_name = r.common_name or self.common_name
+            if r.common_name and not self.common_name:
+                self.common_name = r.common_name
+            elif r.common_name != self.common_name:
+                self.errors.add(
+                    'Common name mismatch: %s != %s'
+                    % (r.common_name, self.common_name))
+            if r.plant_type and not self.plant_type:
+                self.plant_type = r.plant_type
+            elif r.plant_type != self.plant_type:
+                self.errors.add(
+                    'Plant type mismatch: %s != %s'
+                    % (r.plant_type, self.plant_type))
+
             self.invasive = r.invasive == "Yes" or self.invasive
+
+            if r.location:
+                self.locations.add(r.location)
+
             self.photos.append(r.filename)
+
+        if self.common_name is None:
+            self.errors.add('Missing common name')
+
+        if self.plant_type is None:
+            self.errors.add('Missing plant type')
+
+        if len(self.locations) == 0:
+            self.errors.add('Missing location')
+
+
+class PhotoCollection():
+    def __init__(self, raw_records):
+        self.grouped_photos = {}
+        self.unidentified_photos = []
+        self.plant_records = []
+
+        for r in raw_records:
+            photo_record = PhotoRecord(r)
+            key = photo_record.scientific_name
+            if not key:
+                self.unidentified_photos.append(photo_record)
+            else:
+                if key not in self.grouped_photos:
+                    self.grouped_photos[key] = []
+                self.grouped_photos[key].append(photo_record)
+
+        self.scientific_names = list(self.grouped_photos.keys())
+        self.scientific_names.sort()
+
+        for scientific_name in self.scientific_names:
+            self.plant_records.append(
+                PlantRecord(self.grouped_photos[scientific_name]))
+
+    def get_plant_record(self, scientific_name):
+        for r in self.plant_records:
+            if r.scientific_name == scientific_name:
+                return r
+        return None
 
 
 def main():
@@ -37,22 +93,13 @@ def main():
     raw_records = json.load(f)
     f.close()
 
-    grouped_photos = {}
-    unidentified_photos = []
+    photos = PhotoCollection(raw_records)
 
-    for r in raw_records:
-        photo_record = PhotoRecord(r)
-        key = photo_record.scientific_name
-        if not key:
-            unidentified_photos.append(photo_record)
-        else:
-            if key not in grouped_photos:
-                grouped_photos[key] = []
-            grouped_photos[key].append(r)
+    print(json.dumps(photos.scientific_names, indent=1))
 
-    scientific_names = list(grouped_photos.keys())
-    scientific_names.sort()
-    print(json.dumps(scientific_names, indent=1))
+    records = photos.plant_records
+    for r in records:
+        print(r.scientific_name, r.common_name, r.invasive, r.photos)
 
 
 if __name__ == "__main__":
--- book_formatter/tests/test_loading.py
+++ book_formatter/tests/test_loading.py
@@ -1,36 +1,100 @@
 import json
 import pytest
-from book_formatter.book_formatter import PhotoRecord
+from book_formatter.book_formatter import PhotoRecord, PhotoCollection
 
 
-records = json.loads("""[
+@pytest.fixture
+def records():
+    return json.loads("""[
     {"fileName": "1"},
     {"fileName": "2.NEF", "scientificName": "?",
-     "commonName": "foobar", "rating": 3}
+        "commonName": "foobar", "rating": 3},
+    {"fileName": "3.NEF", "scientificName": "Foo bar",
+        "commonName": "foobar", "rating": 4},
+    {"fileName": "4.jpg", "scientificName": "Foo bar",
+        "commonName": "foobarx", "rating": 3, "location": "front yard"},
+    {"fileName": "5.jpg", "scientificName": "Foo bar2",
+        "rating": 3, "location": "back yard"},
+    {"fileName": "6.jpg", "scientificName": "Foo bar2",
+        "rating": 3, "invasive": "No", "location": "front yard"},
+    {"fileName": "7.jpg", "scientificName": "Foo bar2",
+        "rating": 3, "invasive": "Yes", "plantType": "forbs"},
+    {"fileName": "8.NEF", "scientificName": "Foo bar3",
+        "commonName": "foobar", "rating": 4, "plantType": "forbs"},
+    {"fileName": "9.jpg", "scientificName": "Foo bar3",
+        "commonName": "foobarx", "rating": 3, "plantType": "trees"}
 ]""")
 
 
 @pytest.fixture
-def r1():
-    return PhotoRecord(records[0])
+def photos(records):
+    pc = PhotoCollection(records)
+    for r in pc.plant_records:
+        print(r)
+    return PhotoCollection(records)
 
 
-@pytest.fixture
-def r2():
-    return PhotoRecord(records[1])
+def test_filename_no_ext(records):
+    assert PhotoRecord(records[0]).filename == '1.jpg'
 
 
-def test_filename_no_ext(r1):
-    assert r1.filename == '1.jpg'
+def test_filename_ext(records):
+    assert PhotoRecord(records[1]).filename == '2.jpg'
 
 
-def test_filename_ext(r2):
-    assert r2.filename == '2.jpg'
+def test_no_rating(records):
+    assert PhotoRecord(records[0]).rating == 0
 
 
-def test_no_rating(r1):
-    assert r1.rating == 0
+def test_qmark(records):
+    assert not PhotoRecord(records[0]).scientific_name
 
 
-def test_qmark(r1):
-    assert not r1.scientific_name
+def test_unidentified(photos):
+    assert len(photos.unidentified_photos) == 2
+
+
+def test_plant_count(photos):
+    assert len(photos.plant_records) == 3
+
+
+def test_common_name_conflict(photos):
+    r = photos.get_plant_record('Foo bar')
+    assert 'foobar' == r.common_name
+    assert 'Common name mismatch: foobarx != foobar' in r.errors
+
+
+def test_missing_common_name(photos):
+    r = photos.get_plant_record('Foo bar2')
+    assert not r.common_name
+    assert 'Missing common name' in r.errors
+
+
+def test_invasive(photos):
+    assert photos.get_plant_record('Foo bar2').invasive
+
+
+def test_not_invasive(photos):
+    assert not photos.get_plant_record('Foo bar').invasive
+
+
+def test_plant_type(photos):
+    assert "forbs" == photos.get_plant_record('Foo bar2').plant_type
+
+
+def test_missing_plant_type(photos):
+    assert 'Missing plant type' in photos.get_plant_record('Foo bar').errors
+
+
+def test_plant_type_mismatch(photos):
+    r = photos.get_plant_record('Foo bar3')
+    assert 'forbs' == r.plant_type
+    assert 'Plant type mismatch: trees != forbs' in r.errors
+
+
+def test_location(photos):
+    assert 2 == len(photos.get_plant_record('Foo bar2').locations)
+
+
+def test_missing_location(photos):
+    assert 'Missing location' in photos.get_plant_record('Foo bar3').errors
