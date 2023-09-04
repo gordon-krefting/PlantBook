d22557003cd995b0418a5adef26a693ab209466b
Gordon Krefting <gordon@krefting.org>
2023-09-04 09:38:05 -0400

    moved .gitignore to the right place

--- book_formatter/book_formatter/book_formatter.py
+++ book_formatter/book_formatter/book_formatter.py
@@ -4,12 +4,7 @@
 # command line. This will make packaging easier and I can take out the
 # silliness with the __init__.py files.
 # import imagesize
-import json
 import os
-import shutil
-from values import LOCATIONS, PLANT_TYPES
-from jinja2 import Environment, PackageLoader
-from os.path import dirname, join as joinpath
 
 
 OUTPUT_DIR = '/Users/gkreftin/temp/'
@@ -101,31 +96,3 @@ class PhotoCollection():
             if r.scientific_name == scientific_name:
                 return r
         return None
-
-
-def main():
-    f = open(OUTPUT_DIR + 'PhotoBook.json')
-    raw_records = json.load(f)
-    f.close()
-
-    photos = PhotoCollection(raw_records)
-
-    print(json.dumps(photos.scientific_names, indent=1))
-
-    env = Environment(loader=PackageLoader('book_formatter', 'templates'))
-    template = env.get_template('index.jinja2')
-
-    with open(OUTPUT_DIR + 'index.html', 'w') as fh:
-        fh.write(template.render(
-            plants=photos.plant_records,
-            unidentified_photos=photos.unidentified_photos,
-            plant_types=PLANT_TYPES,
-            locations=LOCATIONS,
-        ))
-
-    cssfile = joinpath(dirname(__file__), 'templates', 'plantbook.css')
-    shutil.copyfile(cssfile, OUTPUT_DIR + 'plantbook.css')
-
-
-if __name__ == "__main__":
-    main()
